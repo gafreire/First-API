cacfbe5c02ba415b1fdc10312d2f6847cb788a37
Gabrel Freire <gfreirefumes@gmail.com>
2025-12-16 22:57:24 -0300

    ♻️ Refatorando funções de find para uso do db

--- MyContactsV2/src/repositories/ContactsRepository.js
+++ MyContactsV2/src/repositories/ContactsRepository.js
@@ -20,18 +20,21 @@ let contacts = [
 ];
 
 class ContactRepository {
-  findAll() {
-    return new Promise((resolve) => {
-      resolve(contacts);
-    });
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts')
+    return rows
   }
 
-  findById(id) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.id === id))
-    );
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id])
+    return row;
   }
 
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email])
+    return row;
+  }
+  
   delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
@@ -39,11 +42,6 @@ class ContactRepository {
     });
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.email === email))
-    );
-  }
 
   async create({ name, email, phone, category_id }) {
     const [row] = await db.query(`
