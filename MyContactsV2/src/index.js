a8e25812e8d9782594c1a52a90cbf48357f9668d
Gabrel Freire <gfreirefumes@gmail.com>
2025-12-21 14:58:36 -0300

    ✨ Adicionando middleware para tratamento de erros

--- MyContactsV2/src/index.js
+++ MyContactsV2/src/index.js
@@ -5,5 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes)
+app.use((error, request, response, next) => {
+  console.log(error)
+  response.sendStatus(500)
+})
 
 app.listen(3000, () => console.log("🚀 Server rodando na porta 3000"));
