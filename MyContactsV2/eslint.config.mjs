6cea2a0de0da8c9ebdbf507cc31e0eef1131a694
Gabrel Freire <gfreirefumes@gmail.com>
2025-12-21 14:58:12 -0300

    💄Retirando variavel next do lint

--- MyContactsV2/eslint.config.mjs
+++ MyContactsV2/eslint.config.mjs
@@ -3,6 +3,15 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
+    }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" }
+  },
 ]);
