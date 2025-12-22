df2767570014389d4b70a1a3395c46b6482a43cc
Gabrel Freire <gfreirefumes@gmail.com>
2025-12-21 22:51:04 -0300

    ✨ Finalizando CRUD de categories

--- MyContactsV2/src/repositories/CategoriesRepository.js
+++ MyContactsV2/src/repositories/CategoriesRepository.js
@@ -1,17 +1,44 @@
 const db = require("../database");
 class CategoryRepository {
   async findlAll() {
-    const rows = await db.query('SELECT * FROM categories')
-    return rows
+    const rows = await db.query("SELECT * FROM categories");
+    return rows;
   }
-  async create({ name}) {
-    const [row] = await db.query(`
+
+  async findById(id) {
+    const row = await db.query("SELECT * FROM categories WHERE id = $1", [id]);
+    return row;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(
+      `
       INSERT INTO categories(name)
       VALUES($1)
       RETURNING *
-      `, [name])
-      return row
+      `,
+      [name]
+    );
+    return row;
   }
+
+  async delete(id) {
+    const deleteOp = await db.query("DELETE FROM categories WHERE id = $1", [id]);
+    return deleteOp;
+  }
+
+  async update(id, { name }) {
+      const [row] = await db.query(`
+        UPDATE categories
+        SET name = $1
+        WHERE id = $2
+        RETURNING *
+      `, [name, id]);
+
+      return row;
+    }
+
+
 }
 
-module.exports = new CategoryRepository()
+module.exports = new CategoryRepository();
--- MyContactsV2/src/routes.js
+++ MyContactsV2/src/routes.js
@@ -4,12 +4,15 @@ const CategoryController = require("./app/controllers/Category.controller");
 const router = Router();
 
 router.get("/contacts", ContactController.index)
-router.get("/contacts/:id", ContactController.show)
-router.delete("/contacts/:id", ContactController.delete)
 router.post("/contacts", ContactController.store)
+router.get("/contacts/:id", ContactController.show)
 router.put("/contacts/:id", ContactController.update)
+router.delete("/contacts/:id", ContactController.delete)
 
 router.get("/categories", CategoryController.index)
 router.post("/categories", CategoryController.store)
+router.get("/categories/:id", CategoryController.show)
+router.put("/categories/:id", CategoryController.update)
+router.delete("/categories/:id", CategoryController.delete)
 
 module.exports = router;
